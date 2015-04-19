7352f28e187b39e41ea48ffeaca81a4b9fdf63df
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-04-19 22:53:08 +0300

    Pool and what not

--- library/ListT/Curl.hs
+++ library/ListT/Curl.hs
@@ -1,25 +1,79 @@
 module ListT.Curl where
 
 import BasePrelude hiding (cons, uncons)
-import Foreign
+import Foreign hiding (Pool)
 import MTLPrelude hiding (Error)
 import Control.Monad.Trans.Either hiding (left, right)
 import ListT (ListT)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as BU
+import qualified Data.Pool as DP
 import qualified Network.Curl as Curl
 import qualified ListT as L
+import qualified Language.Haskell.TH.Syntax as TH
 
 
-type Error =
-  Curl.CurlCode
+newtype Pool s =
+  Pool (DP.Pool Curl.Curl)
 
-urlExecutor :: String -> (ListT IO ByteString -> IO a) -> IO (Either Error a)
+usePool :: PoolSettings -> (forall s. ReaderT (Pool s) IO a) -> IO a
+usePool (PoolSettings size timeout) m =
+  Curl.withCurlDo $ bracket acquire release $ runReaderT m . Pool
+  where
+    acquire =
+      DP.createPool Curl.initialize (const (return ())) 1 (fromIntegral timeout) size
+    release pool =
+      DP.destroyAllResources pool
+
+
+-- |
+-- Settings of a pool.
+data PoolSettings =
+  PoolSettings !Int !Int
+  deriving (Show)
+
+instance TH.Lift PoolSettings where
+  lift (PoolSettings a b) = 
+    [|PoolSettings a b|]
+
+-- | 
+-- A smart constructor for pool settings.
+poolSettings :: 
+  Int
+  -- ^
+  -- The maximum number of connections to keep open. 
+  -- The smallest acceptable value is 1.
+  -- Requests for connections will block if this limit is reached.
+  -> 
+  Int
+  -- ^
+  -- The amount of seconds for which an unused connection is kept open. 
+  -- The smallest acceptable value is 1.
+  -> 
+  Maybe PoolSettings
+  -- ^
+  -- Maybe pool settings, if they are correct.
+poolSettings size timeout =
+  if size > 0 && timeout >= 1
+    then Just $ PoolSettings size timeout
+    else Nothing
+
+
+-- |
+-- Presented as a specialised monad to prohibit the concurrent use of a single session handle.
+newtype Session a =
+  Session (ReaderT Curl.Curl IO a)
+  deriving (Functor, Applicative, Monad, MonadIO)
+
+runSession :: Session a -> ReaderT (Pool s) IO a
+runSession (Session m) =
+  ReaderT $ \(Pool pool) -> DP.withResource pool $ runReaderT m
+
+
+urlExecutor :: String -> (ListT IO ByteString -> IO a) -> Session (Either Curl.CurlCode a)
 urlExecutor url handler =
-  Curl.withCurlDo $ do
-    h <- Curl.initialize
-
+  Session $ ReaderT $ \h -> do
     Curl.setopt h $ Curl.CurlFailOnError True
     Curl.setDefaultSSLOpts h $ url
     Curl.setopt h $ Curl.CurlURL url
